a18b4c9fa0ca53154a550222f23da0db99ca0b22
ChristopherTaal <63666767+ChristopherTaal@users.noreply.github.com>
2021-11-25 01:30:44 +0100

    text edited

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,22 +8,22 @@ class Services extends Component {
             {
                 icon:<FaCocktail/>,
                 title:"Free cocktails",
-                info:"It is best to take a moment before ending the day with your favourite cocktails."
+                info:"It is best to take a moment before ending the day with your favourite cocktails. It is our pleasure to serve you. See you."
             },
             {
                 icon:<FaHiking/>,
                 title:"Free hiking",
-                info:"We have team that is well-expirience in your sports. We take advantage of our beautiful mountain that is known in the area."
+                info:"We have team that is well-experience in your sports. We take advantage of our beautiful mountain that is known in the area."
             },
             {
                 icon:<FaShuttleVan/>,
                 title:"Free shuttle van",
-                info:"We cater your transport during happy hours. We offer free transport from our hotel to the closest train atation. Our shuttle is environment friendly."
+                info:"We offer free transport from our hotel to the closest train atation. Our shuttle is environment friendly."
             },
             {
                 icon:<FaBeer/>,
                 title:"Free beers",
-                info:"We make our own beers on the hotel. This is one of the many reason that we are proud of. We welcome our guest that loves beer to take advantage of our free taste during our happy hours at the hotel."
+                info:"We make our own beers on the hotel. We welcome our guest that loves beer to take advantage of our free taste during our happy hours at the hotel."
             }
         ]
     }
